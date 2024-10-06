530cbe5fce11f43264c99269a585b0d46045b8f0
nicolewkm <72010802+nicolewkm@users.noreply.github.com>
2024-10-06 23:19:31 +0000

    Create print nickname command

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("नमस्ते, 世界.")
+    // Check if the number of arguments is correct and therefore returns error if number is missing
+    if len(os.Args) != 2 {
+        fmt.Println("ERROR!")
+        return
+    }
+
+    // Converts argument to an integer and checks it according to conditions
+    count, err := strconv.Atoi(os.Args[1])
+    if err != nil || count < 0 || count > 10 {
+        fmt.Println("ERROR!")
+        return
+    }
+
+    // Loop to print the nickname based on number of times specified
+    for i := 0; i < count; i++ {
+        fmt.Println("gentle-gerbil")  
+    }
 }
